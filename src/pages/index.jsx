66b61c5b92fb390356790dfe80edec349d2ded4d
vijay kumar <admin@bootstrapguru.com>
2022-12-22 10:41:02 -0600

    boilerplate

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -251,7 +251,7 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Vijay Tupakula - Software designer, founder, and amateur astronaut
+          Spencer Sharp - Software designer, founder, and amateur astronaut
         </title>
         <meta
           name="description"
@@ -261,7 +261,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Vijay Software designer, founder, and amateur astronaut.
+            Software designer, founder, and amateur astronaut.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Spencer, a software designer and entrepreneur based in New York
